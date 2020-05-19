04cb4bfd855daf160a109dbba4a472bcf048017c
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-05-19 07:58:17 +0200

    updated to version 2.7.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -6,11 +6,10 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 ## Install QC:DE game mod
 $url = Get-ModdbDlUrl 'https://www.moddb.com/downloads/start/133718'
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName   = "$env:ChocolateyPackageName-mod"
   unzipLocation = $installLocation
   url           = $url
-  softwareName  = 'qc-doom-edition*'
-  checksum      = 'FCAC9FFD6289843EA07E3973925869C417A8FDDBA531F4B7E8D17FB59221B32D'
+  checksum      = '3A8F35765E688F5EB9AA8A39C46D76F68692088A10BFD4EA45F591DDB7FAE054'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
@@ -19,7 +18,7 @@ Install-ChocolateyZipPackage @packageArgs
 ## https://www.moddb.com/mods/quake-champions-doom-edition/addons/qcde-doom-4-skin-pack
 $url = Get-ModdbDlUrl 'https://www.moddb.com/addons/start/170672'
 $packageArgs = @{
-  packageName   = "$env:ChocolateyPackageName" + "D4T"
+  packageName   = "$env:ChocolateyPackageName-d4-skin-pack"
   unzipLocation = $installLocation
   url           = $url
   checksum      = '183A4B5002EC88CFAD0185D258343C387B557CF4B6D6F9EAD279EC008C1A3446'
@@ -31,22 +30,32 @@ Install-ChocolateyZipPackage @packageArgs
 ## https://www.moddb.com/mods/quake-champions-doom-edition/downloads/qcde-hd-portraits
 $url = Get-ModdbDlUrl 'https://www.moddb.com/downloads/start/136555'
 $packageArgs = @{
-  packageName   = "$env:ChocolateyPackageName" + "HD_Portraits"
+  packageName   = "$env:ChocolateyPackageName-hd-portraits"
   unzipLocation = $installLocation
   url           = $url
-  checksum      = '54E7D9831C13B296836511DC2D570A7867B571C37FBA9B1F5F0B5BAB65C45B95'
+  checksum      = '2F735F20031CB4902FE7BC812A99B0DF38BD51A7D375B6F7A9C1D6283E7D047A'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$url = Get-ModdbDlUrl 'https://www.moddb.com/downloads/start/133876'
+$packageArgs = @{
+  packageName   = "$env:ChocolateyPackageName-mp-maps"
+  unzipLocation = $installLocation
+  url           = $url
+  checksum      = '187C097B77048778DBD1203163DD6847D6C5DEF85BCFECF87D4D89EA50CBB871'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
 
 ## Download & Convert game shortcuts icon
-## https://zdoom.org/w/images/thumb/a/af/QCDE_logo.png/120px-QCDE_logo.png
+## https://doomwiki.org/w/images/thumb/a/af/QCDE_logo.png/240px-QCDE_logo.png
 $iconSrc = 'QCDE_logo.png'
 $iconSrcPath = "$(Join-Path $ENV:TEMP $iconSrc)"
 Get-ChocolateyWebFile -PackageName $iconSrc `
   -FileFullPath $iconSrcPath `
-  -Url 'https://cdn.statically.io/img/zdoom.org/w/images/thumb/a/af/QCDE_logo.png/120px-QCDE_logo.png' `
-  -Checksum '9351321A90668178852DA6A43DFA88AA452ECF23A3D156503CDE9EDDCD1D8C98' `
+  -Url 'https://cdn.statically.io/img/doomwiki.org/w/images/thumb/a/af/QCDE_logo.png/240px-QCDE_logo.png' `
+  -Checksum 'BFB51EE411113C140E092B3D586CBAE8593AF501FA0A6DC7D9AE072486BD5DCE' `
   -ChecksumType 'sha256'
 $iconName = 'QCDE_logo.ico'
 $iconPath = "$(Join-Path $toolsDir $iconName)"
@@ -54,14 +63,16 @@ $iconPath = "$(Join-Path $toolsDir $iconName)"
 
 
 ## StartMenu shortcuts
+Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'QCDE Manual 2.7.lnk')" `
+  -TargetPath "$(Join-Path $installLocation 'QCDE Manual 2.7.pdf')"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'QCDE QuickGuide.lnk')" `
   -TargetPath "$(Join-Path $installLocation 'QCDE QuickGuide.txt')"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'QCDE Readme.lnk')" `
   -TargetPath "$(Join-Path $installLocation 'QCDE Readme.txt')"
+Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'QCDE MapList.lnk')" `
+  -TargetPath "$(Join-Path $installLocation 'QCDEmaps MapList')"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'QCDE FAQ.lnk')" `
   -TargetPath "$(Join-Path $installLocation 'QCDE FAQ.txt')"
-Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'QCDE PvE Rebalance (2.5).lnk')" `
-  -TargetPath "$(Join-Path $installLocation 'QCDE PvE Rebalance (2.5).txt')"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'QCDE Changelog.lnk')" `
   -TargetPath "$(Join-Path $installLocation 'QCDE Changelog.txt')"
 
@@ -78,10 +89,10 @@ Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $startMenuDir 'QCDE Do
 ## StartMenu - Multiplayer shortcuts
 $SMMultiplayerDir = "$(Join-Path $startMenuDir 'Multiplayer')"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $SMMultiplayerDir 'QCD2E [MP] startServer [LAN].lnk')" `
-  -TargetPath "$zandronum" -Arguments "$ModPack `"$D4Tsprites`" `"$HDFaces`" -iwad $iWAD2 -host -port 10666" `
+  -TargetPath "$zandronum" -Arguments "$ModPack `"$D4Tsprites`" `"$HDFaces`" `"$MPMaps`" -iwad $iWAD2 -host -port 10666" `
   -WorkingDirectory "$installLocation"
 Install-ChocolateyShortcut -ShortcutFilePath "$(Join-Path $SMMultiplayerDir 'QCD2E [MP] joinServer [LAN].lnk')" `
-  -TargetPath "$zandronum" -Arguments "$ModPack `"$D4Tsprites`" `"$HDFaces`" -iwad $iWAD2 -connect 127.0.0.1:10666" `
+  -TargetPath "$zandronum" -Arguments "$ModPack `"$D4Tsprites`" `"$HDFaces`" `"$MPMaps`" -iwad $iWAD2 -connect 127.0.0.1:10666" `
   -WorkingDirectory "$installLocation"
 
 ## Desktop
--- tools/commonEnv.ps1
+++ tools/commonEnv.ps1
@@ -5,6 +5,8 @@ $GameName = "QuakeChampions Doom Edition"
 $installLocation = "$(Join-Path $toolsDir $GameName)"
 $startMenuDir = [IO.Path]::Combine($ENV:ProgramData, 'Microsoft\Windows\Start Menu\Programs', $GameName)
 
-$ModPack = "QCDEv2.6.1.pk3"
+$ModPack = "QCDEv2.7.pk3"
+$HDFaces = 'QCDE--HDFaces2.7.pk3'
+
 $D4Tsprites = 'QCDE D4T New Sprites v1.1.1.pk3'
-$HDFaces = 'QCDE--HDfaces2.5.pk3'
+$MPMaps = 'QCDEmaps2.7.pk3'
